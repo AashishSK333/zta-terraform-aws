98d664a4d96cc8b130972359c7056bb982ac2871
AashishSK333 <sk9ashish@gmail.com>
2024-03-19 06:54:00 +0800

    zta

--- zta-web-application/main.tf
+++ zta-web-application/main.tf
@@ -1,1 +1,28 @@
 #test file
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+module "web_app_1" {
+  source = "../web-app-module"
+
+  # Input Variables
+  bucket_prefix    = "web-app-1-data"
+  domain           = "devopsdeployed.com"
+  app_name         = "web-app-1"
+  environment_name = "production"
+  instance_type    = "t2.micro"
+  create_dns_zone  = true
+  db_name          = "webapp1db"
+  db_user          = "foo"
+  db_pass          = var.db_pass_1
+}
